56a6ac462af9dde6518ad77f9ca86358115d979c
kashinYing <573255300@qq.com>
2018-01-17 14:52:53 +0800

    add

--- src/index.js
+++ src/index.js
@@ -1,22 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const formatName = user => `${user.firstName} ${user.lastName}`;
-
-const getGreeting = user => {
-  if (user) {
-    return <h1>Hello, {formatName(user)}!</h1>;
-  }
-  return <h1>Hello, Stranger.</h1>;
+const tick = () => {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(element, document.querySelector('#root'));
 };
 
-const user = {
-  firstName: 'Kashin',
-  lastName: 'Liu',
-};
-
-// const element = <h1>Hello, {formatName(user)}!</h1>;\
-
-const element = <h1>{getGreeting()}</h1>;
-
-ReactDOM.render(element, document.querySelector('#root'));
+setInterval(tick, 1000);
